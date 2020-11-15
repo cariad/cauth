4414621aa22bb5a43ed64c51a21082824dbc4f08
Cariad Eccleston <cariad@cariad.me>
2020-11-15 11:52:43 +0000

    Add configuration/__init__.py

---
+++ cauth/configuration/__init__.py
@@ -0,0 +1,8 @@
+# isort: off
+from cauth.configuration.domain import Domain
+from cauth.configuration.environment_variable import EnvironmentVariable
+from cauth.configuration.configuration import Configuration
+
+# isort: on
+
+__all__ = ["Configuration", "Domain", "EnvironmentVariable"]
