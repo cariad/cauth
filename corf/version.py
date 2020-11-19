dfac9c3a4cd375180281f2704cf46a873c38dc05
Cariad Eccleston <cariad@cariad.me>
2020-11-19 07:48:43 +0000

    Support reading version in Python 3.6

--- corf/version.py
+++ corf/version.py
@@ -1,9 +1,18 @@
-import importlib.resources as pkg_resources
+from sys import version_info
+from types import ModuleType
+from typing import cast
+
+if version_info.major == 3 and version_info.minor <= 6:
+    import importlib_resources as resources_legacy
+else:
+    from importlib import resources
 
 
 def get_version() -> str:
     """
     Gets the package version.
     """
-    with pkg_resources.open_text(__package__, "VERSION") as stream:
-        return stream.readline().strip()
+    pkg_resources = resources or cast(ModuleType, resources_legacy)
+    with pkg_resources.open_text(__package__, "VERSION") as stream:  # type: ignore
+        raw = stream.readline()
+    return str(raw.strip())
